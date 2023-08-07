13bb9f136297418922e6c89b99eda2995af06212
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-07 13:06:18 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => {
+        return (
+          <div key={`${actor}${index}`}>
+            <p>{actor.name}</p>
+            <ul>
+              {actor.movies.map((movie, index) => {
+                return <li key={`${movie}${index}`}>{movie}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => {
+        return (
+          <div key={`${director}${index}`}>
+            <p>{director.name}</p>
+            <ul>
+              {director.movies.map((movie, index) => {
+                return <li key={`${movie}${index}`}>{movie}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Directors;
